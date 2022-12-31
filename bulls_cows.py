9a0985481ac3f8b2798b45463747646fd0c215ce
Jonathan W. Cranford <6826390+jwcranford@users.noreply.github.com>
2022-12-31 15:51:27 -0500

    first working version with new algo
    Now eliminates candidates based on the number of bulls
    and cows from each guess

--- bulls_cows.py
+++ bulls_cows.py
@@ -33,55 +33,20 @@ def candidates(alphabet, cell_count):
     else:
         return crossn(alphabet, alphabet, cell_count - 1)
 
-# reduce() takes the current list of candidates and a deduction, 
-# then returns the revised candidates excluded by the deduction
-def reduce(candidates, deduction, cell_count):
-    if len(deduction) == 2:
-        count = int(deduction[0])
-        label = deduction[1]
-        return [w for w in candidates if w.count(label) == count]
-    elif len(deduction) == cell_count:
-        for i in range(0,cell_count):
-            if deduction[i] != '.':
-                candidates = [w for w in candidates if w[i] == deduction[i]]
-    return candidates
-
-# def print_top(candidates, top):
-#     print(f"{len(candidates)} candidates left")
-#     if len(candidates) <= top:
-#         for c in candidates:
-#             print(c)
-
 def print_top(scores, top):
     print(f"{len(scores)} candidates left")
     for (w,s) in scores[0:top]:
         print(f"{w}: {s}")
 
-def next_guess(len):
+def next_guess(squares):
     while True:
-        print(f'Enter next guess and results. Guess must {len} characters long.')
+        print(f'Enter next guess and results. Guess must be {squares} characters long.')
         g = input('Guess: ')
-        if len == len(g):
+        b = input('Bulls: ')
+        c = input('Cows: ')
+        if squares == len(g) and b.isdigit() and c.isdigit():
             break
-    b = input('Bulls: ')
-    c = input('Cows: ')
-    return (g, b, c)
-
-# def process_guess(scores, guess, bulls, cows):
-#     match bulls:
-#         match r:
-#             # If black, Remove all words containing g
-#             case '.':
-#                 words = [w for w in words if not g in w]
-#             # If yellow
-#             #    * Remove all words without g
-#             #    * Remove all words with g in current position
-#             case 'y':
-#                 words = [w for w in words if g in w and w[i] != g]
-#             # If green, Remove all words without g in current position
-#             case 'G':
-#                 words = [w for w in words if w[i] == g]
-#     return words
+    return (g, int(b), int(c))
 
 # returns the number of bulls and cows for the given guess, compared to the answer
 def bulls_cows(answer, guess):
@@ -101,15 +66,16 @@ def bulls_cows(answer, guess):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='''Assistant for bulls and cows game. 
-Prompts for deductions during the game, reduces the solution space, and
-prints out the number of remaining possible solutions''')
+Prompts for guesses and results during the game and restricts the solution space 
+accordingly, printing out the top-scoring remaining solutions. Solutions
+with the least number of unique characters / colors score the highest.''')
     parser.add_argument("alphabet", 
         help="characters to use to fill in the squares")
     parser.add_argument('squares', 
         help='number of squares in each guess', default=3, type=int)
     parser.add_argument('-t', '--top', 
         help='number of top scoring candidates to print',
-        default=10, type=int)
+        default=1, type=int)
 
     args = parser.parse_args()
 
@@ -121,6 +87,5 @@ prints out the number of remaining possible solutions''')
 
     while True:
         print_top(scores, args.top)
-        # deduction = next_deduction()
-        # candidates = reduce(candidates, deduction, args.squares)
-        (guess, bulls, cows)= next_guess()
+        (guess, bulls, cows)= next_guess(args.squares)
+        scores = [(c, s) for (c,s) in scores if bulls_cows(c, guess) == (bulls, cows)]
